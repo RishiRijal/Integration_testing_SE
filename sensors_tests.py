ac0c91821d01f35b30dd39c4605216bf7a1d65d1
RishiRijal <135732124+RishiRijal@users.noreply.github.com>
2024-02-27 09:23:08 +0200

    Update sensors_tests.py

--- sensors_tests.py
+++ sensors_tests.py
@@ -1,19 +1,18 @@
-import unittest
 import sensors_main
+import unittest
 from unittest.mock import patch
+import sys
 
 class TestIntegration(unittest.TestCase):
+
     @patch('builtins.print')
     def test_integration_check_limits(self, mock_print):
-        # Set up
-        sys.argv = ['sensors_main.py', '18', '22']  # Command line parameters
-        expected_output = "21.2, 18.2, 18.2, 22.2\n-5.0, -4.2, -3.9, -4.5\n1.2, 0.0, 0.5, -0.8, -1.0\n25.0, -4.2, -13.9, 4.5\n"
+        sys.argv = ['sensors_main.py', '16', '16']
+        expected_output = "Error: Incorrect command line arguments.\n"
         
-        # Execution
         sensors_main.main()
         output = mock_print.call_args[0][0]
 
-        # Assertion
         self.assertEqual(output, expected_output)
 
 if __name__ == '__main__':
